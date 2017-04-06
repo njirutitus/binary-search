8ccb710e917138c8c83935f97b6d2df0f015ed28
njirutitus <njirutitus@gmail.com>
2017-04-06 20:21:52 +0300

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,40 +1,28 @@
-class BinarySearch(list):
-		def __init__(self,a,b):
-			self.a = a
-			self.b = b
+def binary_search(A, target):
+	lo = 0
+	hi = len(A)
+	count = 0
+	count_index = {}
+	while lo <= hi:
+		count = count + 1
+		mid = lo + (hi-lo)/2
+		mid = int(mid)
+		if A[mid] == target:
+			count_index['count'] = count
+			count_index['index'] = mid
+			return count_index          
+		elif A[mid] < target: 
+			lo = mid+1
+		else:
+			hi = mid-1
+def list(a,b):
+	i = 0
+	j = 0
+	blist = []
+	while i < a:
+		blist.append(i)
+		i = i + 1
+		j = j + b
+	return blist
+	length = len[blist]
 
-		def Search(value):
-			lo = 0
-			A = glist(self.a,self.b)
-			hi = len(A)
-			count = 0
-			count_index = {}
-			while lo <= hi:
-				count = count + 1
-				mid = lo + (hi-lo)/2
-				mid = int(mid)
-				if A[mid] == target:
-					count_index['count'] = count
-					count_index['index'] = mid
-					return count_index          
-				elif A[mid] < target: 
-					lo = mid+1
-				else:
-					hi = mid-1
-class list:
-	def __init__(self,a,b):
-		self.a = a 
-		self.b = b 
-
-	def glist(self): 
-		a = self.a 
-		b = self.b 
-		i = 0
-		j = 0
-		blist = []
-		while i < a:
-			blist.append(i)
-			i = i + 1
-			j = j + b
-		return blist
-		lenght = len[blist]
