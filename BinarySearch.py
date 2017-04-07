8e5bb2aafc7f30276d435dc580aa8f076cd33229
njirutitus <njirutitus@gmail.com>
2017-04-07 10:39:36 +0300

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,28 +1,25 @@
-def BinarySearch(A, target):
-	lo = 0
-	hi = len(A)
-	count = 0
-	count_index = {}
-	while lo <= hi:
-		count = count + 1
-		mid = lo + (hi-lo)/2
-		mid = int(mid)
-		if A[mid] == target:
-			count_index['count'] = count
-			count_index['index'] = mid
-			return count_index          
-		elif A[mid] < target: 
-			lo = mid+1
-		else:
-			hi = mid-1
-def list(a,b):
-	i = 0
-	j = 0
-	blist = []
-	while i < a:
-		blist.append(i)
-		i = i + 1
-		j = j + b
-	return blist
-	length = len[blist]
+class BinarySearch(list):
+		def __init__(self,a,b):
+			self.a = a
+			self.b = b
+			self.array = list.__init__(self,[x for x in range(self.b , (self.a + self.b),self.b)])
+			self.length = len(self)
+
+		def Search(self,target):
+			lo = 0
+			hi = self.length
+			
+			count_index = {}
+			count_index['count'] = 0
+			while lo < hi:
+				count_index['count']  += 1
+				mid = (hi+lo)//2
+				midval = self[mid]
+				if midval < target: 
+					lo = mid+1
+				elif midval > target:
+					hi = mid
+				else:
+					count_index['index'] = mid
+					return count_index
 
